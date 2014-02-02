a09c8270054dcdda6a005c90c1781186309a4299
kjellski <otto.kjell@gmail.com>
2014-02-02 20:39:45 +0100

    solved duplexer

---
+++ duplexer.js
@@ -0,0 +1,7 @@
+var duplexer = require('duplexer')
+  , spawn = require('child_process').spawn;
+
+module.exports = function (cmd, args) {
+  var child = spawn(cmd, args);
+  return duplexer(child.stdin, child.stdout);
+};
